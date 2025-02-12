e165878bc82e50d252b03bd65a89e0ed041efb68
varda04 <62212439+varda04@users.noreply.github.com>
2025-02-12 15:03:44 +0530

    Add files via upload

---
+++ prac3.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import csv
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
+from sklearn.naive_bayes import GaussianNB
+sklearn.__version__
+
+
+# In[2]:
+
+
+#reading the csv dataset
+df= pd.read_csv('/home/student/Downloads/Iris.csv')
+df
+
+
+# In[3]:
+
+
+# #preprocessing
+zero_not_accepted= ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
+print(df)
+for column in zero_not_accepted:
+    df[column]= df[column].replace(0, np.NaN)
+    mean= float(df[column].mean())
+    df[column]= df[column].replace(np.NaN, mean)
+    
+df
+
+
+# In[28]:
+
+
+#splitting columns into input and output
+
+x= df.iloc[:, 1:5]
+y= df.iloc[:, -1:]
+
+
+# In[29]:
+
+
+x
+
+
+# In[30]:
+
+
+y
+
+
+# In[31]:
+
+
+#splitting into training and testing inputs and outputs
+x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.2)
+
+
+# In[32]:
+
+
+print(x_train)
+y_train
+
+
+# In[33]:
+
+
+#Standardizing the input: fitting to normal distribution and transforming
+
+sc= StandardScaler()
+x_sc_train= sc.fit_transform(x_train)
+x_sc_test= sc.fit_transform(x_test)
+
+
+# In[34]:
+
+
+#training the model
+
+model= GaussianNB()
+model.fit(x_sc_test, y_test)
+
+
+# In[35]:
+
+
+#predicting output
+
+y_pred= model.predict(x_sc_test)
+y_pred
+
+
+# In[38]:
+
+
+#Evaluating the model
+
+cm= confusion_matrix(y_test, y_pred)
+accuracy= accuracy_score(y_test, y_pred)
+print("Correct Predictions: ", cm[0][0]+cm[1][1]+cm[2][2])
+print(cm)
+print("Accuracy: ", accuracy)
+
